122ad19c99efee3404d27a73d1eb726fbb4cf65d
Nathan Bellette <nath78@gmail.com>
2017-12-17 23:17:33 +1100

    Added file uploader React component.

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import 'isomorphic-fetch';
-// import * as Dropzone from 'react-dropzone/dist/index';
+import * as Dropzone from 'react-dropzone/dist/index';
 
 export class Home extends React.Component<RouteComponentProps<{}>, {}> {
 
@@ -16,15 +16,27 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
         this.setState({
             files
         }); 
+        console.log(files);
     }
 
     render() {
+
+        const styles = {
+            width: '200px',
+            height: '200px',
+            borderWidth: '2px',
+            borderColor: 'rgb(102, 102, 102)',
+            borderStyle: 'dashed',
+            borderRadius: '5px',
+            padding: '10px'
+        };
+
         return (
             <div>
                 <h1>Upload Images</h1>
-                {/* <Dropzone onDrop={this.onDrop}>
+                 <Dropzone onDrop={this.onDrop} style={styles}>
                     Click or select files to upload.
-                </Dropzone>  */}
+                 </Dropzone>
             </div>
         );
     }
